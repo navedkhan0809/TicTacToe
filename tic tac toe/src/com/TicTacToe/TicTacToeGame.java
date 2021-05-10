9ee8ce6e14554c2f9dadc93c2c361ddc28d34bf8
navedkhan0809 <navedkhan0809@gmail.com>
2021-05-10 10:05:25 +0530

    UC2 added

--- tic tac toe/src/com/TicTacToe/TicTacToeGame.java
+++ tic tac toe/src/com/TicTacToe/TicTacToeGame.java
@@ -7,10 +7,14 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		
 		char[] board = createBoard();
-		
+		Scanner userInput = new Scanner(System.in);
+		char userLetter = chooseUserLetter(userInput);
+		char computerLetter = (userLetter == 'X') ? '0':'O';
 	}
 	
-	//uc1//
+	
+
+	//UC1//
 	private static char[] createBoard() {
 		char[] board = new char[10];
 		for(int i=0;i<board.length;i++) {
@@ -18,5 +22,12 @@ public class TicTacToeGame {
 	}
 	return board;
 	}
-		
+	
+	
+	//UC2//
+	private static char chooseUserLetter(Scanner userInput) {
+		System.out.println("Choose your Letter: ");
+		return userInput.next().toUpperCase().charAt(0);
+	}
+	
 }
